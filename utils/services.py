2f04458bb9b0dec4a684660b5178115a97d21115
Vil <Vil.king.li@mail.ru>
2023-06-19 19:37:14 +0300

    course work 3, version 1

---
+++ utils/services.py
@@ -0,0 +1,60 @@
+import json
+from datetime import datetime
+
+filename = 'data/operation.json'
+
+
+def load_json(filename):
+    with open(filename, 'r', encoding='UTF-8') as f:
+        raw_json_dict = json.load(f)
+
+    json_dict = [item for item in raw_json_dict if item]
+
+    return json_dict
+
+
+def date_format(data_str: str):
+    parsed = datetime.strptime(data_str, "%Y-%m-%dT%H:%M:%S.%f")
+    formatted_date = parsed.strftime("%d.%m.%Y")
+
+    return formatted_date
+
+
+def get_last_five_successful_operations(soft_list: list) -> list:
+    successful_operations = [op for op in soft_list if op["state"] == "EXECUTED"]
+    last_five_successful_operations = successful_operations[:5]
+
+    return last_five_successful_operations
+
+
+def sort_by_date(json_dict=None):
+    sort_list = sorted(json_dict, key=lambda x: x.get("date"), reverse=True)
+    return sort_list
+
+
+def mask_card(operation_credentials: str) -> str:
+    # Значение по ключу
+    if operation_credentials:
+        credentials_name = " ".join(operation_credentials.split(" ")[:-1])
+        credentials_namber = operation_credentials.split(" ")[-1]
+
+        if len(credentials_namber) == 16:
+            # видны первые 6 цифр и последние 4
+            number_hide = credentials_namber[:6] + "*" * 6 + credentials_namber[:-4]
+            # разбито по блокам по 4 цифры
+            number_sep = [number_hide[i:i + 4] for i in range(0, len(credentials_namber), 4)]
+            return f'{credentials_name} {" ".join(number_sep)}'
+
+        elif len(credentials_namber) == 20:
+            return f'{credentials_name} {credentials_namber.replace(credentials_namber[:-4], "**")}'
+    # Если по ключу не было получено значение, вернется строка N/A
+    return "N/A"
+
+
+def print_info(operations):
+    for op in operations:
+        operations_amount: dict = op['operationsAmount']
+        print(f"{date_format(op['date'])} {op['description']}'\n"
+              f"{mask_card(op.get('from'))} -> {mask_card(op.get('to'))}\n"
+              f"{operations_amount.get('amount')} {operations_amount['currency'].get('name')}")
+        print()
